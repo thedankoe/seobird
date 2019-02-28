7454915d28553f2d30d1705d60956e82a13f8ff3
Dan Koe <thedankoe@gmail.com>
2019-02-27 18:13:14 -0700

    live stripe test again

--- src/components/checkoutPackage.js
+++ src/components/checkoutPackage.js
@@ -3,12 +3,9 @@ import { CheckoutButton } from './styles/TextStyles'
 
 const CheckoutPackage = class extends React.Component {
   componentDidMount() {
-    this.stripe = window.Stripe(
-      `${process.env.STRIPE_KEY}` || 'pk_test_p3OHsINOUkTaOhrx6uoohOLo',
-      {
-        betas: ['checkout_beta_4'],
-      }
-    )
+    this.stripe = window.Stripe(process.env.STRIPE_KEY, {
+      betas: ['checkout_beta_4'],
+    })
   }
 
   async redirectToCheckout(event) {
